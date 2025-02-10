d4cc25e4c8bfb5bb614ebba6f9e960a91b639577
keal khondekar <kewalkhondekar25@gmail.com>
2025-02-10 14:01:56 +0530

    feat: clerk middleware added

---
+++ middleware.ts
@@ -0,0 +1,29 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isProtectedRoute = createRouteMatcher(["/dashboard"]);
+const isPublicRoute = createRouteMatcher(["/"]);
+
+export default clerkMiddleware( async (auth, req) => {
+
+  const { userId } = await auth();
+
+  if(userId && isPublicRoute(req)) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  };
+
+  if (!userId && isProtectedRoute(req)) {
+    return NextResponse.redirect(new URL("/sign-in", req.url));
+  }
+
+  if(isProtectedRoute(req)) await auth.protect();
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
